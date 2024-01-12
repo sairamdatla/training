8df5c407a7cee71c3cbf3be59124e41c04952d94
datla sairam <datlasairam24@gmail.com>
2024-01-12 12:55:55 +0530

    sample

---
+++ sample-shell-script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+###########
+#name: sai
+#date: 27th
+#
+#################
+#
+#to print name
+#
+#
+echo "my name is saira"
+
+free -g
+
+nproc
+
+df -h
+
+#create a folder
+mkdir doc
+
+#create twi files, cd doc
+touch w1 w2
